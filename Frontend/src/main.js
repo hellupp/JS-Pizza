f2e83e316e886a3446264f41a09f379bca00bf85
hellupp <77292360+hellupp@users.noreply.github.com>
2021-02-04 18:11:34 +0200

    Lab 4 pizza labels done

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,6 +1,3 @@
-/**
- * Created by chaika on 25.01.16.
- */
 
 $(function(){
     //This code will execute when the page is ready
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,4 @@
-/**
- * Created by Andriy on 10.03.2015.
- */
+
 module.exports = function(grunt) {
     //Налаштування збірки Grunt
     var config = {
